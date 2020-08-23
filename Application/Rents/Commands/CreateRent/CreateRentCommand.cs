502abfc995762e14a8504b9eb067d93cec9ce134
Anastasiagumenuk96 <64774763+Anastasiagumenuk96@users.noreply.github.com>
2020-08-23 08:37:16 +0300

    .

---
+++ Application/Rents/Commands/CreateRent/CreateRentCommand.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Rents.Commands.CreateRent
+{
+    public class CreateRentCommand : IRequest<Guid>
+    {
+        public bool IsConfirmed { get; set; }
+
+        public DateTime StartDataRend { get; set; }
+
+        public DateTime FinishDataRend { get; set; }
+
+        public DateTime DataConfirmed { get; set; }
+
+        public ushort Price { get; set; }
+
+        public Guid CarId { get; set; }
+
+        public Guid LocationId { get; set; }
+
+        public string ApplicationUserId { get; set; }
+    }
+}
