7289ae951380de5a8190b3bae9e92a1ced5d13f9
Anastasiagumenuk96 <64774763+Anastasiagumenuk96@users.noreply.github.com>
2020-08-23 08:58:11 +0300

    .

--- Application/Rents/Commands/CreateRent/CreateRentCommandHandler.cs
+++ Application/Rents/Commands/CreateRent/CreateRentCommandHandler.cs
@@ -1,10 +1,42 @@
-﻿using System;
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Application.Rents.Commands.CreateRent
 {
-    public class CreateRentCommandHandler
+    public class CreateRentCommandHandler : IRequestHandler<CreateRentCommand, Guid>
     {
+        private readonly ICarDbContext _context;
+
+        public CreateRentCommandHandler(ICarDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Guid> Handle(CreateRentCommand request, CancellationToken cancellationToken)
+        {
+            var entity = new Rent
+            {
+                IsConfirmed = request.IsConfirmed,
+                StartDataRend = request.StartDataRend,
+                FinishDataRend = request.FinishDataRend,
+                DataConfirmed = request.DataConfirmed,
+                Price = request.Price,
+                CarId = request.CarId,
+                LocationId = request.LocationId,
+                ApplicationUserId = request.ApplicationUserId
+            };
+
+            _context.Rents.Add(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return entity.Id;
+        }
     }
 }
---
+++ CAR/Controllers/RentController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Rents.Commands.CreateRent;
+using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CAR.Controllers
+{
+    public class RentController : Controller
+    {
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> AddRent()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Guid>> AddRent([FromForm] CreateRentCommand command)
+        {
+            return await Mediator.Send(command);
+        }
+    }
+}
